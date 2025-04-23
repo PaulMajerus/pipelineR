f966e4b877a4cfc274eb73cdc6dc4a3bd83d0db8
PaulMajerus <majerus.paul@protonmail.com>
2025-04-23 16:24:49 +0200

    Ajout de commentaire dans le start pipeline

--- R/start_pipeline.R
+++ R/start_pipeline.R
@@ -16,33 +16,42 @@ start_pipeline <- function(batch_size = 10){
 
   # connect_db()
   con <- connect_db()
-
+print("Connection to the DB")
   # fetch_symbols()
   symbols <- fetch_symbols(con)
-
+print("Symbols fetchs")
   # build_summary_table()
   summaryTable <- build_summary_table()
-
+print("Summary table created")
   # split_batch()
   symbolsBatches <- split_batch(vec = symbols,
                                 batch_size = batch_size)
-
+print("Symbols batched")
   # yahoo_query_data()
   table <- yahoo_query_data(symbolsBatches = symbolsBatches)
 
+print("Yahoo data imported")
   # format_table()
   formattedTable <- format_data(table = table)
+
+print("Yahoo data formatted")
 
   #insert_new_data()
   newObservations <- insert_new_data(con = con,
                                     formattedTable = formattedTable)
 
+  print("New observations compiled")
   #log_summary()
   summaryTable <- log_summary(newObservations = newObservations)
 
+  print("Summary table compiled")
   # push_summary_table()
   push_summary_table(con=con,
                      summaryTable = summaryTable)
+
+  print("Summary table pushed")
   #dbDisconnect
   DBI::dbDisconnect(con)
+
+  print("Congrat, connection is now close and job done.")
 }
